76dcc1d4b3c744c0edcf1a4be9853f7dd3169df8
Brigthain kargong <kbrigthain@gmail.com>
2024-11-29 22:00:15 -0500

    Creates internet gateway

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,7 +1,6 @@
 #--------------------------------------------------------------------
 # VPC - Creates a VPC  to the target account
 #--------------------------------------------------------------------
-######################Creating the vpc#######################################################
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_subnet.cidr_block
   enable_dns_hostnames = true
@@ -11,6 +10,20 @@ resource "aws_vpc" "main" {
   tags = merge(var.common.tags,
     {
       "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.vpc.name}-vpc"
+    }
+  )
+
+}
+
+#--------------------------------------------------------------------
+# VPC - Creates and associates internet gateway to vpc 
+#--------------------------------------------------------------------
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.common.tags,
+    {
+      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.vpc.name}-igw"
     }
   )
 
