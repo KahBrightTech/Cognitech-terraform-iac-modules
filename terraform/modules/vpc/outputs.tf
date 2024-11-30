7b9ba9367b7549de749b28f3dce894e524b47bff
Brigthain kargong <kbrigthain@gmail.com>
2024-11-29 22:02:56 -0500

    Internet gateway

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -6,5 +6,11 @@ output "vpc_arn" {
 
 output "vpc_id" {
   description = "The id of the vpc created"
-  value       = aws_vpc.main.ifd
+  value       = aws_vpc.main.id
+}
+
+
+output "igw_id" {
+  description = "The internet gateway id"
+  value = aws_internet_gateway.main_igw.id 
 }
