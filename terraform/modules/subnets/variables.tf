49ef5bb444e476c2556b3b62d409ba82f94c6379
Brigthain kargong <kbrigthain@gmail.com>
2024-12-09 23:26:19 -0500

    Update on subnets

--- terraform/modules/subnets/variables.tf
+++ terraform/modules/subnets/variables.tf
@@ -8,29 +8,37 @@ variable "common" {
   })
 }
 
-# variable "private_subnets" {
-#   description = "The private subnet variables"
-#   type = object({
-#     private_subnet_name       = string
-#     private_subnet_cidr_block = list(string)
-#     az                        = list(string)
-#   })
-# }
-
-variable "subnets" {
+variable "private_subnets" {
+  description = "The private subnet variables"
   type = object({
-    private_subnets = object({
-      private_subnet_name       = string
-      private_subnet_cidr_block = list(string)
-      az                        = list(string)
-    })
-    public_subnets = object({
-      public_subnet_name       = string
-      public_subnet_cidr_block = list(string)
-      az                       = list(string)
-    })
+    private_subnet_name       = string
+    private_subnet_cidr_block = list(string)
+    az                        = list(string)
   })
 }
+
+variable "public_subnets" {
+  description = "The public subnet variables"
+  type = object({
+    public_subnet_name       = string
+    public_subnet_cidr_block = list(string)
+    az                       = list(string)
+  })
+}
+# variable "subnets" {
+#   type = object({
+#     private_subnets = object({
+#       private_subnet_name       = string
+#       private_subnet_cidr_block = list(string)
+#       az                        = list(string)
+#     })
+#     public_subnets = object({
+#       public_subnet_name       = string
+#       public_subnet_cidr_block = list(string)
+#       az                       = list(string)
+#     })
+#   })
+# }
 
 
 variable "vpc_id" {
