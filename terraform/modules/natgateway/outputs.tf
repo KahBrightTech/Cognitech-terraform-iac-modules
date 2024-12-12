33d1beca9304155a801db61280e5924172d95685
Brigthain kargong <kbrigthain@gmail.com>
2024-12-11 20:43:26 -0500

    ouput for ngw

--- terraform/modules/natgateway/outputs.tf
+++ terraform/modules/natgateway/outputs.tf
@@ -1,4 +1,4 @@
-output "eip_id" {
-  description = "The elastic ip id"
-  value       = aws_eip.eip
+output "ngw_id" {
+  description = "The id of the nat gateways"
+  value       = { for key, nat in aws_nat_gateway.ngw : key => nat.id }
 }
