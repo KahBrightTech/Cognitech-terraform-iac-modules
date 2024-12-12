00c70717dd21375c51fe38c61f830a657cd35385
Brigthain kargong <kbrigthain@gmail.com>
2024-12-11 23:34:50 -0500

    update on ngw

--- terraform/modules/natgateway/main.tf
+++ terraform/modules/natgateway/main.tf
@@ -12,23 +12,23 @@
 #   )
 # }
 
-# resource "aws_nat_gateway" "ngw" {
-#   for_each = { for idx, ngw in var.ngw : ngw.name => ngw }
-
-#   allocation_id = each.value.allocation_id
-#   subnet_id     = each.value.subnet_id
-#   tags = merge(var.common.tags, {
-#     "Name" = "${var.common.account_name}-${var.common.region_prefix}-${each.value.name}-${each.key + 1}"
-#   })
-# }
-
 resource "aws_nat_gateway" "ngw" {
-  for_each      = { for idx, ngw in var.ngw : idx => ngw }
+  for_each = { for idx, ngw in var.ngw : ngw.name => ngw }
+
   allocation_id = each.value.allocation_id
   subnet_id     = each.value.subnet_id
   tags = merge(var.common.tags, {
-    "Name" = "${var.common.account_name}-${var.common.region_prefix}-ngw-${each.key + 1}"
+    "Name" = "${var.common.account_name}-${var.common.region_prefix}-${each.value.name}-${each.key + 1}"
   })
 }
+
+# resource "aws_nat_gateway" "ngw" {
+#   for_each      = { for idx, ngw in var.ngw : idx => ngw }
+#   allocation_id = each.value.allocation_id
+#   subnet_id     = each.value.subnet_id
+#   tags = merge(var.common.tags, {
+#     "Name" = "${var.common.account_name}-${var.common.region_prefix}-ngw-${each.key + 1}"
+#   })
+# }
 
 
