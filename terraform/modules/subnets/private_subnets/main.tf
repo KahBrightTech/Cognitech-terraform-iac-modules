a609045be6d8c7ce63ed9e0ace146294ad5ab539
Brigthain kargong <kbrigthain@gmail.com>
2025-02-18 22:27:33 -0500

    updating subnet

--- terraform/modules/subnets/private_subnets/main.tf
+++ terraform/modules/subnets/private_subnets/main.tf
@@ -18,9 +18,9 @@ resource "aws_subnet" "primary" {
 #--------------------------------------------------------------------
 resource "aws_subnet" "secondary" {
   vpc_id               = var.vpc_id
-  availability_zone    = var.private_subnets.primary_availabilty_zone
-  availability_zone_id = var.private_subnets.primary_availabilty_zone_id
-  cidr_block           = var.private_subnets.primary_cidr_block
+  availability_zone    = var.private_subnets.secondary_availabilty_zone
+  availability_zone_id = var.private_subnets.secondary_availabilty_zone_id
+  cidr_block           = var.private_subnets.secondary_cidr_block
   tags = merge(var.common.tags,
     {
       "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.private_subnets.name}-secondary"
@@ -33,9 +33,9 @@ resource "aws_subnet" "secondary" {
 #--------------------------------------------------------------------
 resource "aws_subnet" "secondary" {
   vpc_id               = var.vpc_id
-  availability_zone    = var.private_subnets.primary_availabilty_zone
-  availability_zone_id = var.private_subnets.primary_availabilty_zone_id
-  cidr_block           = var.private_subnets.primary_cidr_block
+  availability_zone    = var.private_subnets.tertiary_availabilty_zone
+  availability_zone_id = var.private_subnets.tertiary_availabilty_zone_id
+  cidr_block           = var.private_subnets.tertiary_cidr_block
   tags = merge(var.common.tags,
     {
       "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.private_subnets.name}-tertiary"
--- terraform/modules/subnets/public_subnets/main.tf
+++ terraform/modules/subnets/public_subnets/main.tf
@@ -19,9 +19,9 @@ resource "aws_subnet" "primary" {
 #--------------------------------------------------------------------
 resource "aws_subnet" "secondary" {
   vpc_id                  = var.vpc_id
-  availability_zone       = var.public_subnets.primary_availabilty_zone
-  availability_zone_id    = var.public_subnets.primary_availabilty_zone_id
-  cidr_block              = var.public_subnets.primary_cidr_block
+  availability_zone       = var.public_subnets.secondary_availabilty_zone
+  availability_zone_id    = var.public_subnets.secondary_availabilty_zone_id
+  cidr_block              = var.public_subnets.secondary_cidr_block
   map_public_ip_on_launch = true # This is required for public subnets
   tags = merge(var.common.tags,
     {
@@ -35,9 +35,9 @@ resource "aws_subnet" "secondary" {
 #--------------------------------------------------------------------
 resource "aws_subnet" "secondary" {
   vpc_id                  = var.vpc_id
-  availability_zone       = var.public_subnets.primary_availabilty_zone
-  availability_zone_id    = var.public_subnets.primary_availabilty_zone_id
-  cidr_block              = var.public_subnets.primary_cidr_block
+  availability_zone       = var.public_subnets.tertiary_availabilty_zone
+  availability_zone_id    = var.public_subnets.tertiary_availabilty_zone_id
+  cidr_block              = var.public_subnets.tertiary_cidr_block
   map_public_ip_on_launch = true # This is required for public subnets
   tags = merge(var.common.tags,
     {
