01cbe7d21a3ed12aace37328bdf9b93db1945306
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-23 14:10:48 -0400

    Update t transit gateway routes

--- terraform/modules/Transit-gateway-routes/variables.tf
+++ terraform/modules/Transit-gateway-routes/variables.tf
@@ -11,17 +11,24 @@ variable "common" {
 variable "tgw_routes" {
   description = "The transit gateway route variables"
   type = object({
+    name               = string
+    route_table_id     = string
+    vpc_cidr_block     = string
     transit_gateway_id = string
   })
 }
 
-variable "route_table_id" {
-  description = "The id of the route table"
-  type        = string
-}
+# variable "route_table_id" {
+#   description = "The id of the route table"
+#   type        = string
+# }
 
-variable "vpc_cidr_block" {
-  description = "The cidr block for the destination vpc"
-  type        = string
-}
+# variable "vpc_cidr_block" {
+#   description = "The cidr block for the destination vpc"
+#   type        = string
+# }
 
+# variable "transit_gateway_id" {
+#   description = "The id of the transit gateway"
+#   type        = string
+# }
