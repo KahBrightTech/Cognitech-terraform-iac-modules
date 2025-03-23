7c7d0dcf0ed42f28114a9f22827110aba85b8b96
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-23 12:23:51 -0400

    Update to tgw_attachments

--- terraform/modules/Transit-gateway-attachments/main.tf
+++ terraform/modules/Transit-gateway-attachments/main.tf
@@ -2,8 +2,8 @@
 # TGW Route - Creates transit gateway attachment
 #--------------------------------------------------------------------
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_attachment" {
-  subnet_ids         = var.tgw_attachment.subnet_ids
-  transit_gateway_id = var.tgw_attachment.transit_gateway_id
+  subnet_ids         = var.tgw_attachments.subnet_ids
+  transit_gateway_id = var.tgw_attachments.transit_gateway_id
   vpc_id             = var.vpc_id
   tags = merge(var.common.tags,
     {
