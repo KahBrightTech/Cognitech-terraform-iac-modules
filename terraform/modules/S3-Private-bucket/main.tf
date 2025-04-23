e167b6728c60f0a645286407bde0fb7b5343da53
Brigthain Kargong <kbrigthain@gmail.com>
2025-04-22 22:00:52 -0400

    Update to s3 bucket module

--- terraform/modules/S3-Private-bucket/main.tf
+++ terraform/modules/S3-Private-bucket/main.tf
@@ -4,36 +4,10 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "aws_iam_policy_document" "default" {
-  statement {
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_86a86e78734d7c0e",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_EffulgenceTechDataAccess_5be58d87cf2da8e9",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_ReadOnlyAccess_9a4a560bae2c526d"
-      ]
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:PutObjectAcl",
-      "s3:GetObjectAcl",
-      "s3:PutObjectVersionAcl",
-      "s3:GetObjectVersionAcl",
-      "s3:PutObjectVersionTagging",
-      "s3:GetObjectVersionTagging",
-    ]
-
-    resources = [
-      aws_s3_bucket.private.arn,
-      "${aws_s3_bucket.private.arn}/*",
-    ]
-  }
+  override_policy_documents = [
+    replace(replace(replace(replace(replace(file(var.s3.policy), "[[resource_name]]", aws_s3_bucket.private.id), "[[account_number]]", data.aws_caller_identity.current.account_id), "[[region]]", data.aws_region.current.name), "[[account_name]]", var.common.account_name), "[[bucket_arn]]", aws_s3_bucket.private.arn)
+  ]
 }
-
 locals {
   bucket_name = var.s3.name_override != null ? var.s3.name_override : "${var.common.account_name}-${var.common.region_prefix}-${var.s3.name}"
 }
@@ -80,8 +54,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
 
 resource "aws_s3_bucket_policy" "default" {
   bucket = local.bucket_name
+  policy = var.s3.policy
 
-  policy = var.s3.override_policy_document != null && var.s3.override_policy_document != "" ? var.s3.override_policy_document : data.aws_iam_policy_document.default.json
+  # policy = var.s3.override_policy_document != null && var.s3.override_policy_document != "" ? var.s3.override_policy_document : data.aws_iam_policy_document.default.json
 }
 resource "aws_s3_bucket_versioning" "bucket" {
   bucket = local.bucket_name
--- terraform/modules/S3-Private-bucket/outputs.tf
+++ terraform/modules/S3-Private-bucket/outputs.tf
@@ -7,3 +7,8 @@ output "id" {
   description = "The ID of the S3 bucket"
   value       = aws_s3_bucket.private.id
 }
+
+output "policy" {
+  description = "The policy of the S3 bucket"
+  value       = aws_s3_bucket_policy.default.policy
+}
