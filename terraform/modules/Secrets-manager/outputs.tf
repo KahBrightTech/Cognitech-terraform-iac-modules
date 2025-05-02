7eb1c47ec6d8443c6268b134533aa174c1f96186
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-02 15:56:22 -0400

    update on secrets manager output file

--- terraform/modules/Secrets-manager/outputs.tf
+++ terraform/modules/Secrets-manager/outputs.tf
@@ -1,14 +1,15 @@
 output "arn" {
   description = "AWS secrets manager ARN"
-  value       = aws_secretsmanager_secret.secret.arn
+  value       = aws_secretsmanager_secret.secret[count.index].arn
 }
 
 output "id" {
   description = "AWS secrets manager ID"
-  value       = aws_secretsmanager_secret.secret.id
+  value       = aws_secretsmanager_secret.secret[count.index].id
 }
 
 output "name" {
   description = "AWS secrets manager name"
-  value       = aws_secretsmanager_secret.secret.name
+  value       = aws_secretsmanager_secret.secret[count.index].name
 }
+
