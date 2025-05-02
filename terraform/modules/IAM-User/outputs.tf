efa581baedeae5f80c41358ada5d79cdfea3a052
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-02 00:17:42 -0400

    update to outputs

--- terraform/modules/IAM-User/outputs.tf
+++ terraform/modules/IAM-User/outputs.tf
@@ -1,10 +1,1 @@
-output "vpc_id" {
-  description = "The id of the vpc created"
-  value       = aws_vpc.main.id
-}
 
-
-output "igw_id" {
-  description = "The internet gateway id"
-  value       = aws_internet_gateway.main_igw.id
-}
