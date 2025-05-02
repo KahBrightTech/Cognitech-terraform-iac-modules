640b51258ba53252d3cc029058b40ce4d8a0b644
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-02 13:20:38 -0400

    Update to secrets variables

--- terraform/modules/Secrets-manager/variables.tf
+++ terraform/modules/Secrets-manager/variables.tf
@@ -16,6 +16,7 @@ variable "secrets_manager" {
     recovery_window_in_days = optional(number)
     policy                  = optional(string)
     value                   = optional(string)
+    keeper_folder_uid       = optional(string)
   })
   default = null
 }
