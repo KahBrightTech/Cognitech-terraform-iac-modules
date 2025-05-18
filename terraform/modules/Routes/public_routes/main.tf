8e79e22687042c473cdda57bc3963579d7246127
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-18 17:35:31 -0400

    Updating the route tables

--- terraform/modules/Routes/public_routes/main.tf
+++ terraform/modules/Routes/public_routes/main.tf
@@ -12,25 +12,31 @@ resource "aws_route" "public_route" {
   gateway_id             = var.public_routes.public_gateway_id
 }
 
-resource "aws_route_table_association" "primary_public_subnet_association" {
-  subnet_id      = var.public_routes.primary_subnet_id
+# resource "aws_route_table_association" "primary_public_subnet_association" {
+#   subnet_id      = var.public_routes.primary_subnet_id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
+
+# resource "aws_route_table_association" "secondary_public_subnet_association" {
+#   subnet_id      = var.public_routes.secondary_subnet_id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
+
+# resource "aws_route_table_association" "tertiary_public_subnet_association" {
+#   count          = var.public_routes.has_tertiary_subnet == true ? 1 : 0
+#   subnet_id      = var.public_routes.tertiary_subnet_id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
+
+# resource "aws_route_table_association" "quaternary_public_subnet_association" {
+#   count          = var.public_routes.has_quaternary_subnet == true ? 1 : 0
+#   subnet_id      = var.public_routes.quaternary_subnet_id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
+
+
+resource "aws_route_table_association" "public_subnet_associations" {
+  for_each       = toset(var.public_routes.subnet_ids)
+  subnet_id      = each.value
   route_table_id = aws_route_table.public_route_table.id
 }
-
-resource "aws_route_table_association" "secondary_public_subnet_association" {
-  subnet_id      = var.public_routes.secondary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "tertiary_public_subnet_association" {
-  count          = var.public_routes.has_tertiary_subnet == true ? 1 : 0
-  subnet_id      = var.public_routes.tertiary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "quaternary_public_subnet_association" {
-  count          = var.public_routes.has_quaternary_subnet == true ? 1 : 0
-  subnet_id      = var.public_routes.quaternary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
--- terraform/modules/Routes/public_routes/variables.tf
+++ terraform/modules/Routes/public_routes/variables.tf
@@ -13,16 +13,24 @@ variable "public_routes" {
   type = object({
     public_gateway_id      = optional(string)
     destination_cidr_block = string
-    primary_subnet_id      = optional(string)
-    secondary_subnet_id    = optional(string)
-    tertiary_subnet_id     = optional(string)
-    has_tertiary_subnet    = optional(bool, false)
-    quaternary_subnet_id   = optional(string)
-    has_quaternary_subnet  = optional(bool, false)
+    subnet_ids             = list(string)
   })
-
 }
 variable "vpc_id" {
   description = "The vpc id"
   type        = string
 }
+
+# variable "public_routes" {
+#   description = "The routes and route tables to be created"
+#   type = object({
+#     public_gateway_id      = optional(string)
+#     destination_cidr_block = string
+#     primary_subnet_id      = optional(string)
+#     secondary_subnet_id    = optional(string)
+#     tertiary_subnet_id     = optional(string)
+#     has_tertiary_subnet    = optional(bool, false)
+#     quaternary_subnet_id   = optional(string)
+#     has_quaternary_subnet  = optional(bool, false)
+#   })
+# }
