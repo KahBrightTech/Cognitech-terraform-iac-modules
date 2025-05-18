1b46a6fbbb6acc98ef1e2d3e16de3f99a6441d35
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-18 19:34:07 -0400

    Test

--- terraform/modules/Routes/public_routes/variables.tf
+++ terraform/modules/Routes/public_routes/variables.tf
@@ -13,7 +13,12 @@ variable "public_routes" {
   type = object({
     public_gateway_id      = optional(string)
     destination_cidr_block = string
-    subnet_ids             = list(string)
+    primary_subnet_id      = optional(string)
+    secondary_subnet_id    = optional(string)
+    tertiary_subnet_id     = optional(string)
+    has_tertiary_subnet    = optional(bool, false)
+    quaternary_subnet_id   = optional(string)
+    has_quaternary_subnet  = optional(bool, false)
   })
 }
 variable "vpc_id" {
@@ -21,16 +26,4 @@ variable "vpc_id" {
   type        = string
 }
 
-# variable "public_routes" {
-#   description = "The routes and route tables to be created"
-#   type = object({
-#     public_gateway_id      = optional(string)
-#     destination_cidr_block = string
-#     primary_subnet_id      = optional(string)
-#     secondary_subnet_id    = optional(string)
-#     tertiary_subnet_id     = optional(string)
-#     has_tertiary_subnet    = optional(bool, false)
-#     quaternary_subnet_id   = optional(string)
-#     has_quaternary_subnet  = optional(bool, false)
-#   })
-# }
+
