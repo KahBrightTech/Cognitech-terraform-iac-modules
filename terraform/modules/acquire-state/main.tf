c33bdd29a1ac0a40f828b02f6003fc93f544a4df
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-20 23:59:43 -0400

    update on acquire state

--- terraform/modules/acquire-state/main.tf
+++ terraform/modules/acquire-state/main.tf
@@ -7,9 +7,10 @@ data "terraform_remote_state" "states" {
   for_each = (var.tf_remote_states != null) ? { for item in var.tf_remote_states : item.name => item } : {}
   backend  = "s3"
   config = {
-    bucket         = var.vpc_state_bucket
-    key            = var.vpc_state_key
+    bucket         = each.value.bucket_name
+    key            = each.value.bucket_key
     region         = data.aws_region.current.name
-    dynamodb_table = var.vpc_state_dynamodb_table
+    dynamodb_table = each.value.lock_table_name
+    encrpt         = true
   }
 }
