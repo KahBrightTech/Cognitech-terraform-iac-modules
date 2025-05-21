2b4df26b0b304ac24ed3b7621cbaec702b327ead
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-21 00:02:19 -0400

    Update

--- terraform/modules/acquire-state/outputs.tf
+++ terraform/modules/acquire-state/outputs.tf
@@ -3,6 +3,6 @@
 #--------------------------------------------------------------------
 output "remote_tfstates" {
   description = "Remote state backend configuration"
-  value       = length(data.terraform_remote_state, states) > 0 ? data.terraform_remote_state.states : null
+  value       = length(data.terraform_remote_state.states) > 0 ? data.terraform_remote_state.states : null
   sensitive   = true
 }
