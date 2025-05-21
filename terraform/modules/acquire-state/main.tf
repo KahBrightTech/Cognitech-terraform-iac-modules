535b0e19157d68e330532012e446c3f70896d3e2
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-21 00:18:07 -0400

    Update

--- terraform/modules/acquire-state/main.tf
+++ terraform/modules/acquire-state/main.tf
@@ -7,10 +7,10 @@ data "terraform_remote_state" "states" {
   for_each = (var.tf_remote_states != null) ? { for item in var.tf_remote_states : item.name => item } : {}
   backend  = "s3"
   config = {
-    bucket       = each.value.bucket_name
-    key          = each.value.bucket_key
-    region       = data.aws_region.current.name
-    use_lockfile = each.value.lock_table_name
-    encrypt      = true
+    bucket         = each.value.bucket_name
+    key            = each.value.bucket_key
+    region         = data.aws_region.current.name
+    dynamodb_table = each.value.lock_table_name
+    encrypt        = true
   }
 }
