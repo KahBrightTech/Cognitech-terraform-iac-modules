e21baecfaf26869ba67905927d17ed8b655e2f83
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-05 01:13:52 -0400

    Update on subnet routes

--- terraform/modules/Routes/public_routes/main.tf
+++ terraform/modules/Routes/public_routes/main.tf
@@ -1,36 +1,72 @@
-resource "aws_route_table" "public_route_table" {
+#--------------------------------------------------------------------
+# Route table for public subnets
+#--------------------------------------------------------------------
+resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
   tags = merge(var.common.tags,
     {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-public"
+      "Name" = "${var.common.account_name}-primary-public-rt"
     }
   )
 }
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
+
+resource "aws_route_table" "public_secondary" {
+  vpc_id = var.vpc_id
+  tags = merge(var.common.tags,
+    {
+      "Name" = "${var.common.account_name}-secondary-public-rt"
+    }
+  )
+}
+
+resource "aws_route_table" "public_tertiary" {
+  count  = var.public_routes.has_tertiary_subnet == true ? 1 : 0
+  vpc_id = var.vpc_id
+  tags = merge(var.common.tags,
+    {
+      "Name" = "${var.common.account_name}-tertiary-public-rt"
+    }
+  )
+}
+
+resource "aws_route_table" "public_quaternary" {
+  count  = var.public_routes
+  vpc_id = var.vpc_id
+  tags = merge(var.common.tags,
+    {
+      "Name" = "${var.common.account_name}-quaternary-public-rt"
+    }
+  )
+}
+
+#--------------------------------------------------------------------
+# Route table routes and associations for public subnets
+#--------------------------------------------------------------------
+resource "aws_route" "public_route_primary" {
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = var.public_routes.destination_cidr_block
-  gateway_id             = var.public_routes.public_gateway_id
+  nat_gateway_id         = var.public_routes.nat_gateway_id
 }
 
 resource "aws_route_table_association" "primary_public_subnet_association" {
   subnet_id      = var.public_routes.primary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "secondary_public_subnet_association" {
   subnet_id      = var.public_routes.secondary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.public_secondary.id
 }
 
 resource "aws_route_table_association" "tertiary_public_subnet_association" {
   count          = var.public_routes.has_tertiary_subnet == true ? 1 : 0
   subnet_id      = var.public_routes.tertiary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.public_tertiary.id
 }
 
 resource "aws_route_table_association" "quaternary_public_subnet_association" {
   count          = var.public_routes.has_quaternary_subnet == true ? 1 : 0
   subnet_id      = var.public_routes.quaternary_subnet_id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.public_quaternary.id
 }
 
