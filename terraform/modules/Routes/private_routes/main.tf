d5398246eb95d357f9804b8390670539c1f2124d
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-05 22:05:05 -0400

    Update on route table

--- terraform/modules/Routes/private_routes/main.tf
+++ terraform/modules/Routes/private_routes/main.tf
@@ -10,63 +10,12 @@ resource "aws_route_table" "private" {
   )
 }
 
-resource "aws_route_table" "private_secondary" {
-  vpc_id = var.vpc_id
-  tags = merge(var.common.tags,
-    {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.private_routes.vpc_name}-secondary-private-rt"
-    }
-  )
-}
-
-resource "aws_route_table" "private_tertiary" {
-  count  = var.private_routes.has_tertiary_subnet == true ? 1 : 0
-  vpc_id = var.vpc_id
-  tags = merge(var.common.tags,
-    {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.private_routes.vpc_name}-tertiary-private-rt"
-    }
-  )
-}
-
-resource "aws_route_table" "private_quaternary" {
-  count  = var.private_routes.has_quaternary_subnet == true ? 1 : 0
-  vpc_id = var.vpc_id
-  tags = merge(var.common.tags,
-    {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.private_routes.vpc_name}-quaternary-private-rt"
-    }
-  )
-}
-
-#--------------------------------------------------------------------
-# Route table routes and associations for private subnets
-#--------------------------------------------------------------------
-resource "aws_route" "private_route_primary" {
+resource "aws_route" "private_route" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = var.private_routes.destination_cidr_block
   nat_gateway_id         = var.private_routes.nat_gateway_id
 }
 
-resource "aws_route" "private_route_secondary" {
-  route_table_id         = aws_route_table.private_secondary.id
-  destination_cidr_block = var.private_routes.destination_cidr_block
-  nat_gateway_id         = var.private_routes.nat_gateway_id
-}
-
-resource "aws_route" "private_route_tertiary" {
-  count                  = var.private_routes.has_tertiary_subnet == true ? 1 : 0
-  route_table_id         = aws_route_table.private_tertiary[0].id
-  destination_cidr_block = var.private_routes.destination_cidr_block
-  nat_gateway_id         = var.private_routes.nat_gateway_id
-}
-
-resource "aws_route" "private_route_quaternary" {
-  count                  = var.private_routes.has_quaternary_subnet == true ? 1 : 0
-  route_table_id         = aws_route_table.private_quaternary[0].id
-  destination_cidr_block = var.private_routes.destination_cidr_block
-  nat_gateway_id         = var.private_routes.nat_gateway_id
-}
 resource "aws_route_table_association" "primary_private_subnet_association" {
   subnet_id      = var.private_routes.primary_subnet_id
   route_table_id = aws_route_table.private.id
@@ -74,18 +23,18 @@ resource "aws_route_table_association" "primary_private_subnet_association" {
 
 resource "aws_route_table_association" "secondary_private_subnet_association" {
   subnet_id      = var.private_routes.secondary_subnet_id
-  route_table_id = aws_route_table.private_secondary.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "tertiary_private_subnet_association" {
   count          = var.private_routes.has_tertiary_subnet == true ? 1 : 0
   subnet_id      = var.private_routes.tertiary_subnet_id
-  route_table_id = aws_route_table.private_tertiary[0].id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "quaternary_private_subnet_association" {
   count          = var.private_routes.has_quaternary_subnet == true ? 1 : 0
   subnet_id      = var.private_routes.quaternary_subnet_id
-  route_table_id = aws_route_table.private_quaternary[0].id
+  route_table_id = aws_route_table.private.id
 }
 
--- terraform/modules/Routes/public_routes/main.tf
+++ terraform/modules/Routes/public_routes/main.tf
@@ -10,63 +10,12 @@ resource "aws_route_table" "public" {
   )
 }
 
-resource "aws_route_table" "public_secondary" {
-  vpc_id = var.vpc_id
-  tags = merge(var.common.tags,
-    {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.public_routes.vpc_name}--secondary-public-rt"
-    }
-  )
-}
-
-resource "aws_route_table" "public_tertiary" {
-  count  = var.public_routes.has_tertiary_subnet == true ? 1 : 0
-  vpc_id = var.vpc_id
-  tags = merge(var.common.tags,
-    {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.public_routes.vpc_name}--tertiary-public-rt"
-    }
-  )
-}
-
-resource "aws_route_table" "public_quaternary" {
-  count  = var.public_routes.has_quaternary_subnet == true ? 1 : 0
-  vpc_id = var.vpc_id
-  tags = merge(var.common.tags,
-    {
-      "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.public_routes.vpc_name}--quaternary-public-rt"
-    }
-  )
-}
-
-#--------------------------------------------------------------------
-# Route table routes and associations for public subnets
-#--------------------------------------------------------------------
-resource "aws_route" "public_route_primary" {
+resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = var.public_routes.destination_cidr_block
   gateway_id             = var.public_routes.public_gateway_id
 }
 
-resource "aws_route" "public_route_secondary" {
-  route_table_id         = aws_route_table.public_secondary.id
-  destination_cidr_block = var.public_routes.destination_cidr_block
-  gateway_id             = var.public_routes.public_gateway_id
-}
-
-resource "aws_route" "public_route_tertiary" {
-  count                  = var.public_routes.has_tertiary_subnet == true ? 1 : 0
-  route_table_id         = aws_route_table.public_tertiary[0].id
-  destination_cidr_block = var.public_routes.destination_cidr_block
-  gateway_id             = var.public_routes.public_gateway_id
-}
-
-resource "aws_route" "public_route_quaternary" {
-  count                  = var.public_routes.has_quaternary_subnet == true ? 1 : 0
-  route_table_id         = aws_route_table.public_quaternary[0].id
-  destination_cidr_block = var.public_routes.destination_cidr_block
-  gateway_id             = var.public_routes.public_gateway_id
-}
 resource "aws_route_table_association" "primary_public_subnet_association" {
   subnet_id      = var.public_routes.primary_subnet_id
   route_table_id = aws_route_table.public.id
@@ -74,18 +23,18 @@ resource "aws_route_table_association" "primary_public_subnet_association" {
 
 resource "aws_route_table_association" "secondary_public_subnet_association" {
   subnet_id      = var.public_routes.secondary_subnet_id
-  route_table_id = aws_route_table.public_secondary.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "tertiary_public_subnet_association" {
   count          = var.public_routes.has_tertiary_subnet == true ? 1 : 0
   subnet_id      = var.public_routes.tertiary_subnet_id
-  route_table_id = aws_route_table.public_tertiary[0].id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "quaternary_public_subnet_association" {
   count          = var.public_routes.has_quaternary_subnet == true ? 1 : 0
   subnet_id      = var.public_routes.quaternary_subnet_id
-  route_table_id = aws_route_table.public_quaternary[0].id
+  route_table_id = aws_route_table.public.id
 }
 
