ba190fd930fea19050565e7924183e8a36fd45aa
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-05 22:10:57 -0400

    Update on route table

--- terraform/modules/Routes/private_routes/outputs.tf
+++ terraform/modules/Routes/private_routes/outputs.tf
@@ -1,20 +1,4 @@
-output "private_id" {
+output "private_route_table_id" {
   description = "The id of the private route table"
   value       = aws_route_table.private.id
-
-}
-
-output "private_secondary_id" {
-  description = "The id of the secondary private route table"
-  value       = aws_route_table.private_secondary.id
-}
-
-output "private_tertiary_id" {
-  description = "The id of the tertiary private route table"
-  value       = length(aws_route_table.private_tertiary) > 0 ? aws_route_table.private_tertiary[0].id : null
-}
-
-output "private_quaternary_id" {
-  description = "The id of the quaternary private route table"
-  value       = length(aws_route_table.private_quaternary) > 0 ? aws_route_table.private_quaternary[0].id : null
 }
--- terraform/modules/Routes/public_routes/outputs.tf
+++ terraform/modules/Routes/public_routes/outputs.tf
@@ -1,21 +1,5 @@
-output "public_id" {
+output "public_route_table_id" {
   description = "The id of the public route table"
   value       = aws_route_table.public.id
 
-}
-
-output "public_secondary_id" {
-  description = "The id of the secondary public route table"
-  value       = aws_route_table.public_secondary.id
-}
-
-
-output "public_tertiary_id" {
-  description = "The id of the tertiary public route table"
-  value       = length(aws_route_table.public_tertiary) > 0 ? aws_route_table.public_tertiary[0].id : null
-}
-
-output "public_quaternary_id" {
-  description = "The id of the quaternary public route table"
-  value       = length(aws_route_table.public_quaternary) > 0 ? aws_route_table.public_quaternary[0].id : null
 }
