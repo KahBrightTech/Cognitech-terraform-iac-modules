7e7abded653ce06d0fc7835fda8233813d11a8aa
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-09 22:40:50 -0400

    Creating IAM roles and policies

--- terraform/modules/IAM-Roles/main.tf
+++ terraform/modules/IAM-Roles/main.tf
@@ -1,29 +1,81 @@
 #--------------------------------------------------------------------
-# VPC - Creates a VPC  to the target account
+# Data
 #--------------------------------------------------------------------
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc.cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  instance_tenancy     = "default"
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
-  tags = merge(var.common.tags,
-    {
-      Name = "${var.common.account_name}-${var.common.region_prefix}-${var.vpc.name}-vpc"
-    }
-  )
+data "aws_iam_roles" "admin_role" {
+  name_regex  = "AWSReservedSSO_AdministratorAccess_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
 
+data "aws_iam_roles" "network_role" {
+  name_regex  = "AWSReservedSSO_NetworkAdministrator_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+#--------------------------------------------------------------------
+# IAM Policy - Creates IAM policy for the specified IAM role
+#--------------------------------------------------------------------
+
+resource "aws_iam_policy" "policy" {
+  name        = "${var.common.account_name}-${var.common.region_prefix}-${var.iam_role.policy.name}-policy"
+  description = var.iam_role.policy.description
+  path        = var.iam_role.policy.path
+  policy = var.iam_role.policy.custom_policy ? replace(
+    replace(
+      replace(
+        replace(
+          replace(
+            file(var.iam_role.policy.policy),
+            "[[account_number]]", data.aws_caller_identity.account_id,
+          ),
+          "[[account_name_abr]]", var.common.account_name_abr
+        ),
+        "[[region]]", data.aws_region.current.name
+      ),
+      "[[admin_role]]", tolist(data.aws_iam_roles.admin_role.arns)[0]
+    ),
+    "[[network_role]]", tolist(data.aws_iam_roles.network_role.arns)[0]
+  ) : file(var.iam_role.policy.policy)
+
+  tags = merge(var.common.tags, {
+    "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.iam_role.policy.name}-policy"
+  })
 }
 
 #--------------------------------------------------------------------
-# VPC - Creates and associates internet gateway to vpc 
+# IAM Role - Creates IAM role with the specified policy
 #--------------------------------------------------------------------
-resource "aws_internet_gateway" "main_igw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_iam_role" "role" {
+  name        = "${var.common.account_name}-${var.common.region_prefix}-${var.iam_role.name}-role"
+  description = var.iam_role.description
+  path        = var.iam_role.path
+  assume_role_policy = var.iam_role.custom_assume_role_policy ? replace(
+    replace(
+      replace(
+        replace(
+          file(var.iam_role.assume_role_policy),
+          "[[account_number]]", data.aws_caller_identity.current.account_id
+        ),
+        "[[account_name_abr]]", var.common.account_name_abr
+      ),
+      "[[region]]", data.aws_region.current.name
+    ),
+    "[[admin_role]]", tolist(data.aws_iam_roles.admin_role.arns)[0]
+  ) : file(var.iam_role.assume_role_policy)
+  force_detach_policies = var.iam_role.force_detach_policies
+  managed_policy_arns   = var.iam_role.managed_policy_arns
+  max_session_duration  = var.iam_role.max_session_duration
+  permissions_boundary  = var.iam_role.permissions_boundary
+  tags = merge(var.common.tags, {
+    "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.iam_role.policy.name}-role"
+  })
+}
 
-  tags = merge(var.common.tags,
-    {
-      Name = "${var.common.account_name}-${var.common.region_prefix}-${var.vpc.name}-igw"
-    }
-  )
+
+#--------------------------------------------------------------------
+#Attach IAM Policy to Role
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
--- terraform/modules/IAM-Roles/variables.tf
+++ terraform/modules/IAM-Roles/variables.tf
@@ -1,18 +1,33 @@
 variable "common" {
   description = "Common variables used by all resources"
   type = object({
-    global        = bool
-    tags          = map(string)
-    account_name  = string
-    region_prefix = string
+    global           = bool
+    tags             = map(string)
+    account_name     = string
+    region_prefix    = string
+    account_name_abr = optional(string, "")
   })
 }
 
-variable "vpc" {
-  description = "The vpc to be created"
+variable "iam_role" {
+  description = "IAM Role configuration"
   type = object({
-    name       = string
-    cidr_block = string
+    name                      = string
+    description               = optional(string)
+    path                      = optional(string, "/")
+    assume_role_policy        = string
+    custom_assume_role_policy = optional(bool, true)
+    force_detach_policies     = optional(bool, false)
+    managed_policy_arns       = optional(list(string))
+    max_session_duration      = optional(number, 3600)
+    permissions_boundary      = optional(string)
+    policy = optional(object({
+      name          = string
+      description   = optional(string)
+      policy        = string
+      path          = optional(string, "/")
+      custom_policy = optional(bool, true)
+    }))
   })
-
+  default = null
 }
