89149e7ccb05da85b3de444baf316440ab69ad82
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-10 00:12:29 -0400

    Update on instance profiles

--- terraform/modules/EC2-profiles/outputs.tf
+++ terraform/modules/EC2-profiles/outputs.tf
@@ -2,14 +2,7 @@
 # Outputs for EC2 Profiles Module
 #--------------------------------------------------------------------
 output "ec2_iam_instance_profile_id" {
-  description = "The ID of the EC2 IAM instance profile created"
-  value = var.ec2_profiles != null ? {
-    for key, item in aws_iam_instance_profile.ec2_profiles :
-    key => {
-      id   = item.id
-      arn  = item.arn
-      name = item.name
-    }
-  } : null
-
+  description = "The EC2 IAM instance profiles map"
+  value       = var.ec2_profiles != null ? aws_iam_instance_profile.ec2_profiles : null
 }
+
