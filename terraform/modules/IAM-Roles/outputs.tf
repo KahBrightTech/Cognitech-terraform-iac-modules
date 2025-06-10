3c27027e92d7b5a962fd3d963948a4aade93b6e3
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-09 23:21:16 -0400

    Update on outputs

--- terraform/modules/IAM-Roles/outputs.tf
+++ terraform/modules/IAM-Roles/outputs.tf
@@ -1,10 +1,34 @@
-output "vpc_id" {
-  description = "The id of the vpc created"
-  value       = aws_vpc.main.id
+#--------------------------------------------------------------------
+# IAM Policy outputs
+#--------------------------------------------------------------------
+output "iam_policy_id" {
+  description = "The ID of the IAM policy created for the role"
+  value       = aws_iam_policy.policy.id
+}
+output "iam_policy_arn" {
+  description = "The ARN of the IAM policy created for the role"
+  value       = aws_iam_policy.policy.arn
 }
 
+output "aws_iam_policy_name" {
+  description = "The name of the IAM policy created for the role"
+  value       = aws_iam_policy.policy.name
+}
 
-output "igw_id" {
-  description = "The internet gateway id"
-  value       = aws_internet_gateway.main_igw.id
+#--------------------------------------------------------------------
+# IAM Role outputs
+#--------------------------------------------------------------------
+output "iam_role_id" {
+  description = "The ID of the IAM role created"
+  value       = aws_iam_role.role.id
+}
+
+output "iam_role_arn" {
+  description = "The ARN of the IAM role created"
+  value       = aws_iam_role.role.arn
+}
+
+output "aws_iam_role_name" {
+  description = "The name of the IAM role created"
+  value       = aws_iam_role.role.name
 }
