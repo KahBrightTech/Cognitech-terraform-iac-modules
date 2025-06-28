aa32e0cffc79321c26eaa57ae4f869425a4b634b
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-27 22:47:06 -0400

    Update on secrets manager secrets

--- terraform/modules/EC2-key-pair/outputs.tf
+++ terraform/modules/EC2-key-pair/outputs.tf
@@ -4,5 +4,4 @@
 output "name" {
   description = "The name of the generated key pair"
   value       = aws_key_pair.generated_key.key_name
-
 }
--- terraform/modules/Secrets-manager/main.tf
+++ terraform/modules/Secrets-manager/main.tf
@@ -12,10 +12,7 @@ data "secretsmanager_field" "fsx_secrets_password" {
 }
 
 locals {
-  use_external_secrets = (
-    var.secrets_manager.record_folder_uid != null &&
-    trim(var.secrets_manager.record_folder_uid, " \t\n\r") != ""
-  )
+  use_external_secrets = trim(coalesce(var.secrets_manager.record_folder_uid, ""), " \t\n\r") != ""
 
   secret_value = local.use_external_secrets ? jsonencode({
     username = data.secretsmanager_field.fsx_secrets_login[0].value
