404f720d44f93f7a0aec0d8a19b55e655e185d14
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-29 22:12:37 -0400

    Update on route 53 records

--- terraform/modules/Route-53-records/outputs.tf
+++ terraform/modules/Route-53-records/outputs.tf
@@ -3,22 +3,22 @@
 #--------------------------------------------------------------------
 output "record_name" {
   description = "The name of the Route 53 record"
-  value       = length(aws_route53_record.records) > 0 ? aws_route53_record.records[0].name : null
+  value       = aws_route53_record.records.name
 }
 output "record_zone_id" {
   description = "The zone ID of the Route 53 record"
-  value       = length(aws_route53_record.records) > 0 ? aws_route53_record.records[0].zone_id : null
+  value       = aws_route53_record.records.zone_id
 }
 output "record_type" {
   description = "The type of the Route 53 record"
-  value       = length(aws_route53_record.records) > 0 ? aws_route53_record.records[0].type : null
+  value       = aws_route53_record.records.type
 }
 output "record_ttl" {
   description = "The TTL of the Route 53 record"
-  value       = length(aws_route53_record.records) > 0 ? aws_route53_record.records[0].ttl : null
+  value       = aws_route53_record.records.ttl
 }
 output "record_records" {
   description = "The records of the Route 53 record"
-  value       = length(aws_route53_record.records) > 0 ? aws_route53_record.records[0].records : null
+  value       = aws_route53_record.records.records
 }
 
