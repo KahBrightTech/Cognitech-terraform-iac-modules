0ce66e747841097aea1db053f210845cd6f67a2d
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-08 00:16:44 -0400

    Update on alb module

--- terraform/modules/ACM-Public-Certs/main.tf
+++ terraform/modules/ACM-Public-Certs/main.tf
@@ -3,6 +3,10 @@
 #--------------------------------------------------------------------
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+data "aws_route53_zone" "zone" {
+  name         = "example.com"
+  private_zone = false
+}
 
 #--------------------------------------------------------------------
 # Locals
@@ -20,4 +24,26 @@ resource "aws_acm_certificate" "main" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_route53_record" "example" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
 }
