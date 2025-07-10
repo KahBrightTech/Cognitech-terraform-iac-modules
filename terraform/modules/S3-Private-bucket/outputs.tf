36077c8a8ff18463d6189d2c24182ecd759d8669
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-09 22:00:01 -0400

    update on replication

--- terraform/modules/S3-Private-bucket/outputs.tf
+++ terraform/modules/S3-Private-bucket/outputs.tf
@@ -27,18 +27,3 @@ output "versioning" {
   description = "The versioning state of the S3 bucket"
   value       = aws_s3_bucket.private.versioning[0].enabled
 }
-
-output "replication_configuration" {
-  description = "The replication configuration of the S3 bucket"
-  value       = length(aws_s3_bucket_replication_configuration.replication) > 0 ? aws_s3_bucket_replication_configuration.replication[0].rules : null
-}
-
-output "server_side_encryption" {
-  description = "The server-side encryption configuration of the S3 bucket"
-  value       = aws_s3_bucket_server_side_encryption_configuration.bucket.rule[0].apply_server_side_encryption_by_default.sse_algorithm
-}
-
-output "lifecycle_rules" {
-  description = "The lifecycle rules of the S3 bucket"
-  value       = aws_s3_bucket.private.lifecycle_rule
-}
