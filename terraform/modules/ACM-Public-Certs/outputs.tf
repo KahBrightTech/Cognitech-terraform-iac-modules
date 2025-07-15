bd1bc1749148b8608df0c05bc69a0712f4b9a67d
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-14 21:32:23 -0400

    Update on certs

--- terraform/modules/ACM-Public-Certs/outputs.tf
+++ terraform/modules/ACM-Public-Certs/outputs.tf
@@ -17,27 +17,4 @@ output "domain_name" {
   value       = aws_acm_certificate.main.domain_name
 }
 
-output "validation_record_fqdns" {
-  description = "FQDNs of the validation records for the ACM Certificate"
-  value       = aws_acm_certificate_validation.validation.validation_record_fqdns
-}
 
-output "validation_status" {
-  description = "Validation status of the ACM Certificate"
-  value       = aws_acm_certificate_validation.validation.status
-}
-
-output "validation_options" {
-  description = "Validation options for the ACM Certificate"
-  value       = aws_acm_certificate.main.domain_validation_options
-}
-
-output "validation_arn" {
-  description = "ARN of the ACM Certificate validation"
-  value       = aws_acm_certificate_validation.validation.arn
-}
-
-output "validation_domain_name" {
-  description = "Domain name used for validation of the ACM Certificate"
-  value       = aws_acm_certificate_validation.validation.domain_name
-}
