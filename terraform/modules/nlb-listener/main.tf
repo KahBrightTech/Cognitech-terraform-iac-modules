2fb31e3ab5b8decb9518c9e4e3bca845ced7ccdb
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-20 20:04:58 -0400

    Update nlb listener block

--- terraform/modules/nlb-listener/main.tf
+++ terraform/modules/nlb-listener/main.tf
@@ -8,14 +8,14 @@ data "aws_region" "current" {}
 # Load Balancer Configuration. Creates default listener for the Load Balancer.
 #-------------------------------------------------------------------------------------------------------------------
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = var.listener.load_balancer_arn
-  port              = var.listener.port
-  protocol          = var.listener.protocol
-  ssl_policy        = var.listener.ssl_policy
-  certificate_arn   = var.listener.certificate_arn
+  load_balancer_arn = var.nlb_listener.load_balancer_arn
+  port              = var.nlb_listener.port
+  protocol          = var.nlb_listener.protocol
+  ssl_policy        = var.nlb_listener.ssl_policy
+  certificate_arn   = var.nlb_listener.certificate_arn
   # Dynamic block for forward default action
   dynamic "default_action" {
-    for_each = var.listener.forward != null ? [var.listener.forward] : []
+    for_each = var.nlb_listener.forward != null ? [var.nlb_listener.forward] : []
     content {
       type             = "forward"
       target_group_arn = default_action.value.target_group_arn
@@ -37,11 +37,11 @@ resource "aws_lb_listener" "listener" {
 # Create Default target group for the Load Balancer
 #-------------------------------------------------------------------------------------------------------------------
 resource "aws_lb_target_group" "default" {
-  count    = var.listener.target_group != null ? 1 : 0
-  name     = var.listener.name
-  port     = var.listener.port
-  protocol = var.listener.protocol
-  vpc_id   = var.listener.vpc_id
+  count    = var.nlb_listener.target_group != null ? 1 : 0
+  name     = var.nlb_listener.name
+  port     = var.nlb_listener.port
+  protocol = var.nlb_listener.protocol
+  vpc_id   = var.nlb_listener.vpc_id
 
   health_check {
     enabled             = var.target_group.health_check.enabled
@@ -55,7 +55,7 @@ resource "aws_lb_target_group" "default" {
   }
 
   tags = merge(var.common.tags, {
-    Name = "${var.common.account_name}-${var.common.region_prefix}-tg-${var.listener.name}"
+    Name = "${var.common.account_name}-${var.common.region_prefix}-tg-${var.nlb_listener.name}"
   })
 }
 
@@ -64,8 +64,8 @@ resource "aws_lb_target_group" "default" {
 #-------------------------------------------------------------------------------------------------------------------
 
 resource "aws_lb_target_group_attachment" "default" {
-  count            = var.listener.target_group && var.listener.target_group.attachment != null ? 1 : 0
+  count            = var.nlb_listener.target_group && var.nlb_listener.target_group.attachment != null ? 1 : 0
   target_group_arn = aws_lb_target_group.default.arn
-  target_id        = var.listener.target_group.attachment.target_id
-  port             = var.listener.target_group.attachment.port
+  target_id        = var.nlb_listener.target_group.attachment.target_id
+  port             = var.nlb_listener.target_group.attachment.port
 }
