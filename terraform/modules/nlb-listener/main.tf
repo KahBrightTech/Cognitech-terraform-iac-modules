2a4b10cef6ae4cb2c7d43d6db76b76723765857d
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-22 00:06:46 -0400

    Update

--- terraform/modules/nlb-listener/main.tf
+++ terraform/modules/nlb-listener/main.tf
@@ -31,7 +31,7 @@ resource "aws_lb_listener" "nlb_listener" {
   certificate_arn   = var.nlb_listener.protocol == "TLS" ? var.nlb_listener.certificate_arn : null
   default_action {
     type             = var.nlb_listener.action
-    target_group_arn = module.nlb_target_group.target_group_arn
+    target_group_arn = module.nlb_target_group[0].target_group_arn
   }
   tags = merge(
     var.common.tags,
