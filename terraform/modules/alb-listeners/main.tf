41b512b49f699ef068bc13ffb42185f04d9f1dde
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-30 22:54:55 -0400

    alb listeners

--- terraform/modules/alb-listeners/main.tf
+++ terraform/modules/alb-listeners/main.tf
@@ -13,10 +13,10 @@ module "alb_target_group" {
 
   common = var.common
   target_group = merge(
-    try(var.alb_listener.target_group, {}),
+    var.alb_listener.target_group,
     {
       vpc_id = var.alb_listener.vpc_id
-      name   = try(var.alb_listener.target_group.name, "${var.common.account_name_abr}-${var.common.region_prefix}-alb-${var.alb_listener.protocol}-${var.alb_listener.port}")
+      name   = var.alb_listener.target_group != null ? var.alb_listener.target_group.name : "${var.common.account_name_abr}-${var.common.region_prefix}-alb-${var.alb_listener.protocol}-${var.alb_listener.port}"
     }
   )
 }
