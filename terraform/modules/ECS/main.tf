5e3798e68f8b91c42c0a142d86156fc5992f56b7
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-08-29 13:59:02 -0400

    ecs creation

---
+++ terraform/modules/ECS/main.tf
@@ -0,0 +1,16 @@
+#--------------------------------------------------------------------
+# Data
+#--------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_iam_roles" "admin_role" {
+  name_regex  = "AWSReservedSSO_AdministratorAccess_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
+data "aws_iam_roles" "network_role" {
+  name_regex  = "AWSReservedSSO_NetworkAdministrator_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
