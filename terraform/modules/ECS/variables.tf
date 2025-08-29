07cde759cc921e8ddc561b0469389d105aaf8b94
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-08-29 15:53:37 -0400

    Update

--- terraform/modules/ECS/variables.tf
+++ terraform/modules/ECS/variables.tf
@@ -9,3 +9,25 @@ variable "common" {
   })
 }
 
+variable "iam_role" {
+  description = "IAM Role configuration"
+  type = object({
+    name                      = string
+    description               = optional(string)
+    path                      = optional(string, "/")
+    assume_role_policy        = string
+    custom_assume_role_policy = optional(bool, true)
+    force_detach_policies     = optional(bool, false)
+    managed_policy_arns       = optional(list(string))
+    max_session_duration      = optional(number, 3600)
+    permissions_boundary      = optional(string)
+    policy = optional(object({
+      name          = string
+      description   = optional(string)
+      policy        = string
+      path          = optional(string, "/")
+      custom_policy = optional(bool, true)
+    }))
+  })
+  default = null
+}
