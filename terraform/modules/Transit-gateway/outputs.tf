c775ab7303e41caa6001c123c91ed201b2578184
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-10-09 22:59:38 -0400

    tgw output

--- terraform/modules/Transit-gateway/outputs.tf
+++ terraform/modules/Transit-gateway/outputs.tf
@@ -6,4 +6,11 @@ output "transit_gateway_id" {
   value       = aws_ec2_transit_gateway.main.id
 }
 
+output "tgw_arn" {
+  description = "The arn of the Transit Gateway"
+  value       = aws_ec2_transit_gateway.main.arn
+}
+
+
+
 
