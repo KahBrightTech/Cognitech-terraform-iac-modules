bbbdf527327380fca01fb4d22a915cc7f5ecbb52
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-10-16 23:28:27 -0400

    tgw attachments

--- terraform/modules/Transit-gateway-attachments/outputs.tf
+++ terraform/modules/Transit-gateway-attachments/outputs.tf
@@ -16,16 +16,16 @@ output "tgw_attachment_arn" {
 #--------------------------------------------------------
 output "ram_resource_share_arn" {
   description = "The ARN of the RAM resource share (if enabled)"
-  value       = var.tgw_attachments.ram.enabled ? aws_ram_resource_share.main[0].arn : null
+  value       = var.tgw_attachments.ram != null ? (var.tgw_attachments.ram.enabled ? aws_ram_resource_share.main[0].arn : null) : null
 }
 
 output "ram_resource_share_id" {
   description = "The ID of the RAM resource share (if enabled)"
-  value       = var.tgw_attachments.ram.enabled ? aws_ram_resource_share.main[0].id : null
+  value       = var.tgw_attachments.ram != null ? (var.tgw_attachments.ram.enabled ? aws_ram_resource_share.main[0].id : null) : null
 }
 
 output "ram_sharing_enabled" {
   description = "Whether RAM sharing is enabled for this TGW attachment"
-  value       = var.tgw_attachments.ram.enabled
+  value       = var.tgw_attachments.ram != null ? var.tgw_attachments.ram.enabled : false
 }
 
