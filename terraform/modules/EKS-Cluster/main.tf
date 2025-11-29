7f17c32c0591978315ca6a919ca49ed4fb33b6b0
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-11-28 21:55:11 -0500

    creating eks cluster

--- terraform/modules/EKS-Cluster/main.tf
+++ terraform/modules/EKS-Cluster/main.tf
@@ -1,4 +1,4 @@
-#--------------------------------------------------------------------
+#-------------------------------------------------------------------
 # Data
 #--------------------------------------------------------------------
 data "aws_caller_identity" "current" {}
@@ -21,4 +21,56 @@ resource "aws_eks_cluster" "eks_cluster" {
   })
 }
 
+#--------------------------------------------------------------------
+# OIDC Provider for EKS Cluster
+#--------------------------------------------------------------------
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = var.eks_cluster.oidc_thumbprints
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
 
+#--------------------------------------------------------------------
+# Key Pair Resource
+#--------------------------------------------------------------------
+
+resource "tls_private_key" "key" {
+  count     = var.eks_cluster.key_pair.is_this_ec2_node_group ? 1 : 0
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "aws_key_pair" "generated_key" {
+  count      = var.eks_cluster.key_pair.is_this_ec2_node_group ? 1 : 0
+  key_name   = var.eks_cluster.key_pair.name
+  public_key = tls_private_key.key[0].public_key_openssh
+  tags = merge(var.common.tags,
+    {
+      Name = "${var.common.account_name}-${var.common.region_prefix}-${var.eks_cluster.key_pair.name}"
+    }
+  )
+}
+
+
+resource "aws_secretsmanager_secret" "private_key_secret" {
+  count                          = var.eks_cluster.key_pair.is_this_ec2_node_group && var.eks_cluster.key_pair.create_secret ? 1 : 0
+  name_prefix                    = "${var.common.account_name}-${var.common.region_prefix}-${var.eks_cluster.key_pair.secret_name}"
+  description                    = var.eks_cluster.key_pair.secret_description
+  recovery_window_in_days        = 7
+  force_overwrite_replica_secret = true
+  policy                         = var.eks_cluster.key_pair.policy
+
+  tags = merge(var.common.tags,
+    {
+      Name = "${var.common.account_name}-${var.common.region_prefix}-${var.eks_cluster.key_pair.secret_name}"
+    }
+  )
+}
+
+
+resource "aws_secretsmanager_secret_version" "private_key_secret_version" {
+  count         = var.eks_cluster.key_pair.is_this_ec2_node_group && var.eks_cluster.key_pair.create_secret ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.private_key_secret[0].id
+  secret_string = tls_private_key.key[0].private_key_pem
+}
--- terraform/modules/EKS-Cluster/outputs.tf
+++ terraform/modules/EKS-Cluster/outputs.tf
@@ -1,3 +1,8 @@
+
+output "eks_cluster_certificate_authority_data" {
+  description = "The certificate authority data for the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
 output "eks_cluster_id" {
   description = "EKS Cluster name."
   value       = aws_eks_cluster.eks_cluster.id
@@ -11,4 +16,16 @@ output "eks_cluster_endpoint" {
 output "eks_cluster_arn" {
   description = "EKS Cluster ARN."
   value       = aws_eks_cluster.eks_cluster.arn
+}
+
+
+output "name" {
+  description = "The name of the generated key pair"
+  value       = length(aws_key_pair.generated_key) > 0 ? aws_key_pair.generated_key[0].key_name : null
+}
+
+
+output "secret_arn" {
+  description = "The ARN of the created Secrets Manager secret (if created)"
+  value       = length(aws_secretsmanager_secret.private_key_secret) > 0 ? aws_secretsmanager_secret.private_key_secret[0].arn : null
 }
