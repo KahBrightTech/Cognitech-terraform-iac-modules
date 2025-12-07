bd575898511c6407581715c0a1922476f715278d
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-12-06 22:07:39 -0500

    cluster update

--- terraform/modules/EKS-Cluster/main.tf
+++ terraform/modules/EKS-Cluster/main.tf
@@ -39,7 +39,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 #--------------------------------------------------------------------
 resource "aws_eks_access_entry" "admin_role" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
-  principal_arn = data.aws_iam_roles.admin_role.arns[0]
+  principal_arn = tolist(data.aws_iam_roles.admin_role.arns)[0]
   type          = "STANDARD"
 }
 
