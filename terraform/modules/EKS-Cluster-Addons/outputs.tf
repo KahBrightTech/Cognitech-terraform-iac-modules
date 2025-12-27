6f9f4e266b51b8338aa72ebe51704a3cb7ce7f84
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-12-26 21:48:23 -0500

    ok updated

--- terraform/modules/EKS-Cluster-Addons/outputs.tf
+++ terraform/modules/EKS-Cluster-Addons/outputs.tf
@@ -1,32 +1,29 @@
-
 output "coredns_addon_id" {
   description = "ID of the CoreDNS addon."
-  value       = aws_eks_addon.coredns.id
+  value       = length(aws_eks_addon.coredns) > 0 ? aws_eks_addon.coredns[0].id : null
 }
 
 output "coredns_addon_version" {
   description = "Version of the CoreDNS addon."
-  value       = aws_eks_addon.coredns.addon_version
+  value       = length(aws_eks_addon.coredns) > 0 ? aws_eks_addon.coredns[0].addon_version : null
 }
 
 output "metrics_server_addon_id" {
   description = "ID of the Metrics Server addon."
-  value       = aws_eks_addon.metrics_server.id
+  value       = length(aws_eks_addon.metrics_server) > 0 ? aws_eks_addon.metrics_server[0].id : null
 }
 
 output "metrics_server_addon_version" {
   description = "Version of the Metrics Server addon."
-  value       = aws_eks_addon.metrics_server.addon_version
+  value       = length(aws_eks_addon.metrics_server) > 0 ? aws_eks_addon.metrics_server[0].addon_version : null
 }
 
 output "secrets_manager_csi_driver_addon_id" {
   description = "ID of the Secrets Manager CSI Driver addon."
-  value       = aws_eks_addon.secrets_manager_csi_driver.id
+  value       = length(aws_eks_addon.secrets_manager_csi_driver) > 0 ? aws_eks_addon.secrets_manager_csi_driver[0].id : null
 }
 
 output "secrets_manager_csi_driver_addon_version" {
   description = "Version of the Secrets Manager CSI Driver addon."
-  value       = aws_eks_addon.secrets_manager_csi_driver.addon_version
+  value       = length(aws_eks_addon.secrets_manager_csi_driver) > 0 ? aws_eks_addon.secrets_manager_csi_driver[0].addon_version : null
 }
-
-
