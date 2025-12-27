73247a2ddeed49ea1aa9f387cf0713c0fec71638
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-12-26 19:23:00 -0500

    cw

--- terraform/modules/EKS-Cluster-Addons/outputs.tf
+++ terraform/modules/EKS-Cluster-Addons/outputs.tf
@@ -19,21 +19,6 @@ output "metrics_server_addon_version" {
   value       = aws_eks_addon.metrics_server.addon_version
 }
 
-output "cloudwatch_observability_addon_id" {
-  description = "ID of the CloudWatch Observability addon."
-  value       = try(aws_eks_addon.cloudwatch_observability[0].id, null)
-}
-
-output "cloudwatch_observability_addon_version" {
-  description = "Version of the CloudWatch Observability addon."
-  value       = try(aws_eks_addon.cloudwatch_observability[0].addon_version, null)
-}
-
-output "cloudwatch_observability_addon_arn" {
-  description = "ARN of the CloudWatch Observability addon."
-  value       = try(aws_eks_addon.cloudwatch_observability[0].arn, null)
-}
-
 output "secrets_manager_csi_driver_addon_id" {
   description = "ID of the Secrets Manager CSI Driver addon."
   value       = aws_eks_addon.secrets_manager_csi_driver.id
@@ -44,12 +29,4 @@ output "secrets_manager_csi_driver_addon_version" {
   value       = aws_eks_addon.secrets_manager_csi_driver.addon_version
 }
 
-output "cloudwatch_observability_addon_arn" {
-  description = "ARN of the CloudWatch Observability addon."
-  value       = try(aws_eks_addon.cloudwatch_observability[0].arn, null)
-}
 
-output "cloudwatch_observability_addon_version" {
-  description = "Version of the CloudWatch Observability addon."
-  value       = try(aws_eks_addon.cloudwatch_observability[0].addon_version, null)
-}
