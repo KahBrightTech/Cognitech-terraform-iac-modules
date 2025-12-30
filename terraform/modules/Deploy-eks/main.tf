a2801e20741d903d49ff69a37c90652c11daae85
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-12-29 22:57:05 -0500

    adding ebs csi driver

--- terraform/modules/Deploy-eks/main.tf
+++ terraform/modules/Deploy-eks/main.tf
@@ -165,19 +165,15 @@ resource "aws_eks_addon" "cloudwatch_observability" {
   depends_on = [module.eks_node_group]
 }
 
-# resource "aws_eks_addon" "secrets_manager_csi_driver" {
-#   count                       = var.eks.eks_addons != null && var.eks.eks_addons.enable_secrets_manager_csi_driver && var.eks.create_node_group ? 1 : 0
-#   cluster_name                = aws_eks_cluster.eks_cluster.name
-#   addon_name                  = "secrets-store-csi-driver"
-#   addon_version               = var.eks.eks_addons.secrets_manager_csi_driver_version
-#   resolve_conflicts_on_update = "PRESERVE"
+resource "aws_eks_addon" "ebs_csi_driver" {
+  count         = var.eks.eks_addons != null && var.eks.eks_addons.enable_ebs_csi_driver && var.eks.create_node_group ? 1 : 0
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  addon_name    = "aws-ebs-csi-driver"
+  addon_version = var.eks.eks_addons.ebs_csi_driver_version
 
-#   tags = merge(var.common.tags, {
-#     "Name" = "${var.common.account_name}-${var.common.region_prefix}-${var.eks.key}-secrets-store-csi-driver-addon"
-#   })
-#   depends_on = [module.eks_node_group]
-# }
-
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+}
 resource "aws_eks_addon" "privateca_issuer" {
   count                       = var.eks.eks_addons != null && var.eks.eks_addons.enable_privateca_issuer && var.eks.create_node_group ? 1 : 0
   cluster_name                = aws_eks_cluster.eks_cluster.name
