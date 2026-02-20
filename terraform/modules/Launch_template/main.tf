a4bd0ec7226b573c49f0f631d65a2ff29e805957
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2026-02-19 23:40:17 -0500

    update on ami

--- terraform/modules/Launch_template/main.tf
+++ terraform/modules/Launch_template/main.tf
@@ -31,6 +31,7 @@ locals {
     # EKS Optimized AMIs
     EKSAL2    = { pattern = "amazon-eks-node-*", owners = ["amazon"] }
     EKSAL2023 = { pattern = "amazon-eks-node-al2023-x86_64-standard-*", owners = ["amazon"] }
+    ECSAL2023 = { pattern = "al2023-ami-ecs-hvm-*", owners = ["amazon"] }
 
     # Red Hat Enterprise Linux AMIs
     RHEL9  = { pattern = "RHEL-9.6.0_HVM_*-x86_64-*-Hourly2-GP3", owners = ["amazon"] }
