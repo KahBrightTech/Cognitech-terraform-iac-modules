80e5440e937660d277c17f9c8070017360808e30
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2026-02-21 00:53:16 -0500

    tg

--- terraform/modules/alb-listeners/outputs.tf
+++ terraform/modules/alb-listeners/outputs.tf
@@ -3,19 +3,31 @@
 #--------------------------------------------------------------------
 output "alb_target_group_arn" {
   description = "The ARN of the ALB target group"
-  value       = var.alb_listener.action == "forward" ? module.alb_target_group[0].target_group_arn : null
+  value = var.alb_listener.action == "forward" ? (
+    length(module.alb_target_group) > 0
+    ? module.alb_target_group[0].target_group_arn
+    : var.alb_listener.target_group_arn
+  ) : null
 }
 
 output "alb_target_group_id" {
   description = "The ID of the ALB target group"
-  value       = var.alb_listener.action == "forward" ? module.alb_target_group[0].target_group_id : null
+  value = var.alb_listener.action == "forward" ? (
+    length(module.alb_target_group) > 0
+    ? module.alb_target_group[0].target_group_id
+    : var.alb_listener.target_group_arn
+  ) : null
 }
 
 output "alb_tg_attachments" {
   description = "The attachments of the ALB target group"
-  value       = var.alb_listener.action == "forward" && var.alb_listener.target_group != null ? (var.alb_listener.target_group.attachments != null ? module.alb_target_group[0].target_group_attachments : null) : null
-
+  value = var.alb_listener.action == "forward" && length(module.alb_target_group) > 0 ? (
+    var.alb_listener.target_group != null && var.alb_listener.target_group.attachments != null
+    ? module.alb_target_group[0].target_group_attachments
+    : null
+  ) : null
 }
+
 #--------------------------------------------------------------------
 # ALB Listener Outputs
 #--------------------------------------------------------------------
@@ -28,5 +40,3 @@ output "alb_listener_id" {
   description = "The ID of the ALB listener"
   value       = aws_lb_listener.alb_listener.id
 }
-
-
