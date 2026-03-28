e9391c8eb4c4b3f674dc21b11701679baf898f74
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2026-03-27 21:39:50 -0400

    update on outputs

--- terraform/modules/Event-Bridge/outputs.tf
+++ terraform/modules/Event-Bridge/outputs.tf
@@ -1,6 +1,6 @@
 output "event_bus_arn" {
-  description = "The ARN of the EventBridge event bus."
-  value       = aws_cloudwatch_event_bus.this.arn
+  description = "The ARN of the EventBridge event bus. If a custom bus is not created, returns the default bus ARN."
+  value       = length(aws_cloudwatch_event_bus.this) > 0 ? aws_cloudwatch_event_bus.this[0].arn : format("arn:aws:events:%s:%s:event-bus/default", data.aws_region.current.name, data.aws_caller_identity.current.account_id)
 }
 
 output "event_rule_arn" {
