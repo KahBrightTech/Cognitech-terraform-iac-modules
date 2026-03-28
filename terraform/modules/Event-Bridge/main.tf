dafd5722f3d40d3b41b8e8c7579149022d219628
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2026-03-27 21:33:16 -0400

    removing ecr block

--- terraform/modules/Event-Bridge/main.tf
+++ terraform/modules/Event-Bridge/main.tf
@@ -30,29 +30,4 @@ resource "aws_cloudwatch_event_target" "this" {
   # No tags argument for aws_cloudwatch_event_target
 }
 
-resource "aws_ecr_replication_configuration" "replication" {
-  count = var.ecr.replication_configuration != null ? 1 : 0
-
-  replication_configuration {
-    dynamic "rule" {
-      for_each = var.ecr.replication_configuration.rules
-      content {
-        dynamic "destination" {
-          for_each = rule.value.destinations
-          content {
-            region      = destination.value.region
-            registry_id = destination.value.registry_id
-          }
-        }
-        dynamic "repository_filter" {
-          for_each = rule.value.repository_filter != null ? [rule.value.repository_filter] : []
-          content {
-            filter      = repository_filter.value.filter
-            filter_type = repository_filter.value.filter_type
-          }
-        }
-      }
-    }
-  }
-}
 
