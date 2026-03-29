d5e6bae0940709a7b1f01e67a3a61594c1d07b00
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2026-03-28 23:22:48 -0400

    lambda invocations

--- terraform/modules/Lambda-invocations/outputs.tf
+++ terraform/modules/Lambda-invocations/outputs.tf
@@ -1,14 +1,14 @@
-output "lambda_permission_statement_ids" {
-  description = "The statement IDs of the Lambda permissions."
-  value       = { for k, v in aws_lambda_permission.this : k => v.statement_id }
+output "lambda_permission_statement_id" {
+  description = "The statement ID of the Lambda permission."
+  value       = aws_lambda_permission.this.statement_id
 }
 
-output "lambda_permission_actions" {
-  description = "The actions granted by the Lambda permissions."
-  value       = { for k, v in aws_lambda_permission.this : k => v.action }
+output "lambda_permission_action" {
+  description = "The action granted by the Lambda permission."
+  value       = aws_lambda_permission.this.action
 }
 
-output "lambda_permission_principals" {
-  description = "The principals allowed by the Lambda permissions."
-  value       = { for k, v in aws_lambda_permission.this : k => v.principal }
+output "lambda_permission_principal" {
+  description = "The principal allowed by the Lambda permission."
+  value       = aws_lambda_permission.this.principal
 }
